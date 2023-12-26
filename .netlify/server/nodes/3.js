f493909aabaec0323f0081fd5044a6fdb24e1054
Jon Ellwood <jonellwood@gmail.com>
2023-12-26 15:36:21 -0500

    migration to SvelteKit 2

--- .netlify/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/about/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.9fba62a4.js","_app/immutable/chunks/index.fec7332a.js"];
-export const stylesheets = ["_app/immutable/assets/3.e67044ec.css"];
-export const fonts = [];
